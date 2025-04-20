e2ee77fa2be5824560510e613d168caef2c6c145
Denis <dvoron10@gmail.com>
2025-04-20 19:47:52 +0200

    update upgrades buttons logic

--- Assets/Scripts/UI/DepartmentMenu/Upgrades/BuyUpgradeButton.cs
+++ Assets/Scripts/UI/DepartmentMenu/Upgrades/BuyUpgradeButton.cs
@@ -2,7 +2,7 @@ using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AddWorkstationButton : MonoBehaviour
+public class BuyUpgradeButton : MonoBehaviour
 {
     [SerializeField] private Button _button;
     [SerializeField] private Department _department;
@@ -10,10 +10,15 @@ public class AddWorkstationButton : MonoBehaviour
 
     private void Awake()
     {
+        if (upgradeId == null)
+        {
+            Debug.LogError($"{nameof(upgradeId)} is null");
+            return;
+        }
         _button.OnClickAsObservable().Subscribe(_ =>
         {
             UpgradeService upgradeService = ServiceLocator.Get<UpgradeService>();
-            upgradeService.ApplyUpgrade(UpgradeDataSO.UpgradeType.DepartmentWorkstationAdd, _department);
+            upgradeService.PurchaseUpgrade(upgradeId, _department);
         }).AddTo(this);
     }
 }
