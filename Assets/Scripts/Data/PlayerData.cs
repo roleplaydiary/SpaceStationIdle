b37ddc54578fd309e3dff4921f6a2b62064b87df
Denis <dvoron10@gmail.com>
2025-04-20 14:54:07 +0200

    add research points

--- Assets/Scripts/Data/PlayerData.cs
+++ Assets/Scripts/Data/PlayerData.cs
@@ -6,18 +6,20 @@ using UniRx;
 public class PlayerData
 {
     public ReactiveProperty<float> playerCredits { get; private set; }
+    public ReactiveProperty<int> researchPoints { get; private set; }
 
-    // Конструктор для инициализации ReactiveProperty
     public PlayerData()
     {
         playerCredits = new ReactiveProperty<float>();
+        researchPoints = new ReactiveProperty<int>();
     }
 
     public Dictionary<string, object> ToDictionary()
     {
         return new Dictionary<string, object>
         {
-            { "playerCredits", playerCredits.Value }
+            { "playerCredits", playerCredits.Value },
+            { "researchPoints", researchPoints.Value }
         };
     }
 
@@ -26,6 +28,8 @@ public class PlayerData
         PlayerData playerData = new PlayerData();
         if (dict.TryGetValue("playerCredits", out object credits))
             playerData.playerCredits.Value = Convert.ToSingle(credits);
+        if (dict.TryGetValue("researchPoints", out object points))
+            playerData.researchPoints.Value = Convert.ToInt32(points);
         return playerData;
     }
 }
