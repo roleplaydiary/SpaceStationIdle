da425765ee4a3b9fd433eac5cda644ea549e8093
Denis <dvoron10@gmail.com>
2025-04-21 14:59:54 +0200

    update department menu with Cargo

--- Assets/Scripts/UI/DepartmentMenu/DepartmentProductionPanelController.cs
+++ Assets/Scripts/UI/DepartmentMenu/DepartmentProductionPanelController.cs
@@ -31,12 +31,23 @@ public class DepartmentProductionPanelController : MonoBehaviour
                 ScienceProductivityPanelInit();
                 break;
             }
+
+            case Department.Cargo:
+            {
+                CargoProductivityPanelInit();
+                break;
+            }
         }
     }
     
     private void BridgeProductivityPanelInit()
     {
         CloseAllPanels();
+        if (bridgeDepartmentProductionViewer == null)
+        {
+            Debug.LogError("Bridge production viewer is null");
+            return;
+        }
         bridgeDepartmentProductionViewer.gameObject.SetActive(true);
         bridgeDepartmentProductionViewer.Initialize();
     }
@@ -44,6 +55,12 @@ public class DepartmentProductionPanelController : MonoBehaviour
     private void EngineerProductivityPanelInit()
     {
         CloseAllPanels();
+        if (engineeringDepartmentProductionViewer == null)
+        {
+            Debug.LogError("Engineering production viewer is null");
+            return;
+        }
+
         engineeringDepartmentProductionViewer.gameObject.SetActive(true);
         engineeringDepartmentProductionViewer.Initialize();
     }
@@ -51,8 +68,25 @@ public class DepartmentProductionPanelController : MonoBehaviour
     private void ScienceProductivityPanelInit()
     {
         CloseAllPanels();
+        if (scienceDepartmentProductionViewer == null)
+        {
+            Debug.LogError("Science production viewer is null");
+            return;
+        }
         scienceDepartmentProductionViewer.gameObject.SetActive(true);
         scienceDepartmentProductionViewer.Initialize();
+    }
+    
+    private void CargoProductivityPanelInit()
+    {
+        CloseAllPanels();
+        if (cargoDepartmentProductionViewer == null)
+        {
+            Debug.LogError("Cargo production viewer is null");
+            return;
+        }
+        cargoDepartmentProductionViewer.gameObject.SetActive(true);
+        cargoDepartmentProductionViewer.Initialize();
     }
 
     private void CloseAllPanels()
@@ -60,7 +94,7 @@ public class DepartmentProductionPanelController : MonoBehaviour
         bridgeDepartmentProductionViewer.gameObject.SetActive(false);
         engineeringDepartmentProductionViewer.gameObject.SetActive(false);
         scienceDepartmentProductionViewer.gameObject.SetActive(false);
-        // cargoDepartmentProductionViewer.gameObject.SetActive(false);
+        cargoDepartmentProductionViewer.gameObject.SetActive(false);
         // medbayDepartmentProductionViewer.gameObject.SetActive(false);
         // securityDepartmentProductionViewer.gameObject.SetActive(false);
         // kitchenDepartmentProductionViewer.gameObject.SetActive(false);
