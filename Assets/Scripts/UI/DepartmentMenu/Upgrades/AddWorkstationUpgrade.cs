50e55b5cc084e94bd81416bcdda24d47bb93ab9e
Denis <dvoron10@gmail.com>
2025-04-25 18:26:40 +0200

    add new workstation upgrade initialization

---
+++ Assets/Scripts/UI/DepartmentMenu/Upgrades/AddWorkstationUpgrade.cs
@@ -0,0 +1,18 @@
+public class AddWorkstationUpgrade : BuyUpgradeButton
+{
+    protected override void Initialize()
+    {
+        var upgrade = ServiceLocator.Get<DataLibrary>().upgradeData.GetUpgradeById(upgradeId);
+        var stationController = ServiceLocator.Get<StationController>();
+
+        var departmentData = stationController.StationData.DepartmentData;
+        var isUpgradeAvailable = false;
+        if (departmentData.TryGetValue(_department, out var data))
+        {
+            isUpgradeAvailable = data.WorkBenchesInstalled == upgrade.value - 1 
+                                 && stationController.StationData.IsUnlocked(_department);
+        }
+
+        gameObject.SetActive(isUpgradeAvailable);
+    }
+}
