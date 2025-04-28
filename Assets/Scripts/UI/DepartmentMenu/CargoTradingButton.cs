4a49261eed548673af2830eeb3fc835a72e41a05
Denis <dvoron10@gmail.com>
2025-04-28 19:11:24 +0200

    Add cargo trade button

---
+++ Assets/Scripts/UI/DepartmentMenu/CargoTradingButton.cs
@@ -0,0 +1,38 @@
+using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CargoTradingButton : MonoBehaviour
+{
+    [SerializeField] private GameObject backgroundActive;
+    [SerializeField] private GameObject backgroundInActive;
+    [SerializeField] private Button _button;
+    private void Start()
+    {
+        var crewService = ServiceLocator.Get<CrewManager>();
+        crewService.workingCrew.ObserveCountChanged().Subscribe(value =>
+        {
+            Initialize();
+        }).AddTo(this);
+        
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        var stationController = ServiceLocator.Get<StationController>();
+        if (stationController.StationData.DepartmentData[Department.Cargo].CrewAtWork > 4)
+        {
+            backgroundActive.SetActive(true);
+            backgroundInActive.SetActive(false);
+            _button.interactable = true;
+        }
+        else
+        {
+            backgroundActive.SetActive(false);
+            backgroundInActive.SetActive(true);
+            _button.interactable = false;
+        }
+    }
+}
